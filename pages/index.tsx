d6bf2d0a11696b49e54abc048b9fb52993116dec
NFTStreetkid <149784942+NFTStreetkid@users.noreply.github.com>
2023-11-06 20:11:49 +0000

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,13 @@ const Home: NextPage = () => {
       >
         {loading ? "Generating..." : "Generate NFT"}
       </Web3Button>
+      import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+const myElement = <h1>I Love JSX!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
       {nfts && (
         <div className={styles.nfts}>
           {nfts.map((nft) => (
